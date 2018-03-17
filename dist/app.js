d17254504884049cf47213bf45c38e5b90e396be
Paul Brzeski <mail@paulbrzeski.com>
2018-03-18 01:16:52 +0800

    Interaction using dat.gui

--- dist/app.js
+++ dist/app.js
@@ -3,11 +3,38 @@
 ( function( $, THREE, window, document ) {
 
   $(document).ready(function() {
-    ImageTracer.imageToSVG(
-      '/demo.jpg',
-      function(svgstr){ ImageTracer.appendSVGString( svgstr, 'svgcontainer' ); },
-      'Posterized2'
-    );
+    var controls = ImageTracer.checkoptions();
+    controls.ltres = 0.1;
+    controls.qtres = 1;
+    controls.strokewidth = 5;
+    controls.viewbox = true;
+
+    var change = function() {
+      $('#svgcontainer').html('<div class="ui active centered inline loader">Loading</div>');
+      ImageTracer.imageToSVG(
+        '/demo.jpg',
+        function(svgstr) {
+          $('#svgcontainer').html('');
+          ImageTracer.appendSVGString( svgstr, 'svgcontainer' );
+        },
+        controls
+      );
+    };
+
+    var gui = new dat.GUI();
+    for (var controlName in controls) {
+      if (isNaN(controls[controlName])) {
+        gui.add(controls, controlName)
+          .onFinishChange(function(){ change(); });
+      }
+      else {
+        var max = controls[controlName] * 2;
+        gui.add(controls, controlName, 0, max)
+          .onFinishChange(function(){ change(); });
+      }
+    }
+
+    change();
   });
 
 } )( jQuery, THREE, window, document );
