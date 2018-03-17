b9d2c48162da8e89696b87de2522b2dd5126ba6c
Paul Brzeski <mail@paulbrzeski.com>
2018-03-18 02:34:26 +0800

    Starting to draw SVG on Three.JS.. not sure what I'm doing wrong

--- dist/app.js
+++ dist/app.js
@@ -4,18 +4,17 @@
 
   $(document).ready(function() {
     var controls = ImageTracer.checkoptions();
-    controls.ltres = 0.1;
-    controls.qtres = 1;
+    controls.colorsampling = 0;
     controls.strokewidth = 5;
     controls.viewbox = true;
 
     var change = function() {
-      $('#svgcontainer').html('<div class="ui active centered inline loader">Loading</div>');
+      $('#svg-preview').html('<div class="ui active centered inline loader">Loading</div>');
       ImageTracer.imageToSVG(
         '/demo.jpg',
         function(svgstr) {
-          $('#svgcontainer').html('');
-          ImageTracer.appendSVGString( svgstr, 'svgcontainer' );
+          $('#svg-preview').html('');
+          ImageTracer.appendSVGString( svgstr, 'svg-preview' );
         },
         controls
       );
@@ -35,6 +34,57 @@
     }
 
     change();
+
+    $('button.fluid.ui.button').click(function() {
+      var $container = $('#model-preview');
+      var scene = new THREE.Scene();
+      var width = $container.parent().innerWidth();
+      var height = 400;
+      window.camera = new THREE.PerspectiveCamera( 50, width / height, 1, 1000 );
+      camera.position.set( 20, 20, 20 );
+      camera.lookAt( 0, 0, 0 );
+      var renderer = new THREE.WebGLRenderer({ alpha: true });
+
+      renderer.setSize( width, height );
+      $container.html( renderer.domElement );
+
+      var loader = new THREE.SVGLoader();
+      var paths = loader.parse($('#svg-preview').html());
+      var group = new THREE.Group();
+      group.scale.multiplyScalar( 10.1 );
+      group.scale.y *= -1;
+      for ( var i = 0; i < paths.length; i ++ ) {
+        var path = paths[ i ];
+        var material = new THREE.MeshBasicMaterial( {
+          color: Math.random() * 0xffffff,
+          polygonOffset: true,
+          polygonOffsetUnits: - i
+        } );
+        var shapes = path.toShapes( true );
+        for ( var j = 0; j < shapes.length; j ++ ) {
+          var shape = shapes[ j ];
+          var geometry = new THREE.ShapeBufferGeometry( shape );
+          var mesh = new THREE.Mesh( geometry, material );
+          group.add( mesh );
+        }
+      }
+      scene.add( group );
+      var box = new THREE.BoxHelper( group, 0xffff00 );
+      scene.add( box );
+
+      var axesHelper = new THREE.AxesHelper( 5 );
+      scene.add( axesHelper );
+
+      var controls = new THREE.OrbitControls( camera, renderer.domElement );
+      function animate() {
+        requestAnimationFrame( animate );
+        render();
+      }
+      function render() {
+        renderer.render( scene, camera );
+      }
+      animate();
+    });
   });
 
 } )( jQuery, THREE, window, document );
