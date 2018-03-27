83f6bbeb0ff400f23b5865b660b22b4c3c1fcccd
Paul Brzeski <mail@paulbrzeski.com>
2018-03-27 23:16:14 +0800

    - Added upload ability - Removed Pickle Rick :'(

--- src/app.js
+++ src/app.js
@@ -6,7 +6,20 @@
   $(document).ready(function() {
     var gui = new dat.GUI();
 
-    $('.js-change-image').click(function(){
+    $('#image_select').click(function(e){
+      $('#image_input').click();
+      e.preventDefault();
+    });
+
+    window.URL = window.URL || window.webkitURL || window.mozURL;
+    $('#image_input').change(function(){
+      var url = URL.createObjectURL(this.files[0]);
+      console.log(url);
+      $('<div class="item"><div class="ui white compact button js-change-image"><div class="ui fluid image mini"><img src="' + url + '" /></div></div></div>')
+        .insertBefore('.ui.inverted.top.fixed.menu .item:last-child');
+    });
+
+    $('.ui.inverted.top.fixed.menu').on('click', '.js-change-image', function(){
       $('#original-image').attr('src', $(this).find('img').attr('src'));
       $('#svg-preview, #potrace-preview').html('<div class="ui active centered inline loader"></div>');
       var callback = function(){
