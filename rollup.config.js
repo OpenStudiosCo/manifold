cf712244bde32b12432b12ebf70f73a6ea1876e0
Paul Brzeski <pbrzeski@doghouse.agency>
2018-03-30 15:59:32 +0800

    - Added Backbone - Starting to work on models

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,14 @@ import buble from 'rollup-plugin-buble';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
-  external: [ '$d3g', 'dat', 'jQuery', 'ImageTracer', 'THREE', 'Potrace' ],
+  external: [ '$d3g', 'Backbone', 'dat', 'jQuery', 'ImageTracer', 'THREE', 'Potrace' ],
   input: 'src/app.js',
   output: {
   	file: 'dist/app.js',
   	format: 'iife',
     globals: {
       '$d3g': '$d3g',
+      Backbone: 'Backbone',
       dat: 'dat',
       jQuery: 'jQuery',
       ImageTracer: 'ImageTracer',
---
+++ src/models.js
@@ -0,0 +1,7 @@
+import Backbone from 'Backbone';
+
+/**
+  * Model definitions.
+  */
+
+export var models = new Backbone.Model.extend();
